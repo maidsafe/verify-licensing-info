185d0aa87d833d46a2508a868fc8de4bd3b9342e
Chris O'Neil <chriso83@protonmail.com>
2022-05-22 06:42:17 +0100

    feat: convert action to nodejs
    Extending the Bash code to support Cargo workspace repositories was getting out of hand, so I
    decided to convert the action to Node. I should have probably written it this way to begin with, but
    quite frankly I just forgot it was something you could do.
    
    This first step is to verify the license files and to make sure they all use the same license. It
    works for both a single-crate repository and a workspace. The verification of the source files
    containing a copyright header will follow in another commit.

--- README.md
+++ README.md
@@ -1,20 +1,52 @@
 # Verify Licensing Info
 
-A little custom action for verifying licensing information in a repository, particularly a Rust
-repository.
+A custom action for verifying licensing information in a repository, particularly a Rust repository.
 
-So far, it verifies:
+Example usage for a single-crate repository:
+```
+- name: install rust
+  id: toolchain
+  uses: actions-rs/toolchain@v1
+  with:
+    profile: minimal
+    toolchain: stable
+    override: true
+- name: install ripgrep
+  run: cargo install ripgrep
+- uses: maidsafe/verify-licensing-info@main
+  name: verify licensing
+  with:
+    company-name: MaidSafe
+```
 
-* The repo contains a LICENSE file.
-* The README has a reference to a license.
-* The README and LICENSE files use the same license.
-* All Rust source files contain a copyright notice.
-* All Rust source files contain a reference to the license reported by the API.
+Example usage for a workspace repository:
+```
+- name: install rust
+  id: toolchain
+  uses: actions-rs/toolchain@v1
+  with:
+    profile: minimal
+    toolchain: stable
+    override: true
+- name: install ripgrep
+  run: cargo install ripgrep
+- uses: maidsafe/verify-licensing-info@main
+  name: verify licensing
+  with:
+    company-name: MaidSafe
+    cargo-workspace: true
+    crates: "sn_interface sn_dysfunction sn_node sn_client sn_api sn_cli"
+```
+
+For verifying source files, the action relies on an installation of [ripgrep](https://crates.io/crates/ripgrep), hence the installation of the Rust toolchain to run the `cargo install`.
+
+It verifies:
+
+* The repository contains a LICENSE file.
+* The README has a reference to a license and it matches the license from the LICENSE file.
 * The Cargo manifest contains a license and it matches the license from the LICENSE file.
+* All Rust source files contain a copyright notice with a license attribution.
 
-Initially there was also an idea to check that all these license references were the same as what
-was reported by the Github API, but unfortunately the Github API always detects what's on the main
-branch, so you can't use this as a CI check; if you change your license, it will fail.
-
-Right now, it only works with a repository that's a single crate. It'll need to be modified to use a
-workspace, which will come soon.
+If the repository is a Cargo workspace, all specified crates will be checked to make sure their
+README and Cargo manifest matches the license in the LICENSE file at the root of the repository.
+This means we wouldn't allow for crates in the same workspace to use different licenses.
---
+++ index.js
@@ -0,0 +1,224 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const fs = require('fs');
+const jq = require('node-jq');
+
+var IS_CARGO_WORKSPACE = false;
+var CRATES = [];
+var COMPANY_NAME = '';
+
+async function runLicensee(sourcePath) {
+  let output = '';
+  const options = {};
+  options.listeners = {
+    stdout: (data) => {
+      output += data.toString();
+    }
+  };
+  await exec.exec(
+    'docker',
+    [
+      'run', '--rm', '--volume',
+      `${sourcePath}:/usr/src/target`, 'licensee', 'detect',
+      '/usr/src/target', '--json'
+    ],
+    options);
+  return output;
+}
+
+async function parseInputs() {
+  IS_CARGO_WORKSPACE = core.getBooleanInput('cargo-workspace', { required: false });
+  if (IS_CARGO_WORKSPACE) {
+    CRATES = core.getInput('crates', { required: false }).split(' ');
+    if (CRATES.length == 0) {
+      core.setFailed('If this repository is a workspace, the crates input must be\
+        set to a space delimited set of crates.');
+      return;
+    }
+  }
+  COMPANY_NAME = core.getInput('company-name', { required: true });
+  core.debug(`cargo-workspace input: ${IS_CARGO_WORKSPACE}`);
+  core.debug(`crates input: ${CRATES}`);
+  core.debug(`company-name input: ${COMPANY_NAME}`);
+}
+
+async function buildLicensee() {
+  await exec.exec('git clone https://github.com/jacderida/licensee');
+  const options = {};
+  options.cwd = 'licensee'
+  await exec.exec('git', ['checkout', 'add-user-to-docker'], options);
+  await exec.exec('docker', ['build', '.', '--tag', 'licensee'], options);
+}
+
+async function matchLicenseFiles(sourcePath) {
+  let licenseeOutput;
+  let jqOutput;
+  try {
+    licenseeOutput = await runLicensee(sourcePath);
+    jqOutput = await jq.run(
+      '.matched_files[].filename', licenseeOutput, { input: 'string', output: 'json' });
+  } catch (err) {
+    core.setFailed(`Action failed: ${err}`);
+    return;
+  }
+  // jq includes the " characters around the filenames, which we don't need.
+  let matchedFiles = jqOutput.split('\n').map(filename => filename.replace(/\"/g, ''));
+  return matchedFiles
+}
+
+async function matchLicenseReferences(sourcePath) {
+  let licenseeOutput;
+  let jqOutput;
+  try {
+    licenseeOutput = await runLicensee(sourcePath);
+    jqOutput = await jq.run(
+      '.matched_files[].matched_license', licenseeOutput, { input: 'string', output: 'json' });
+  } catch (err) {
+    core.setFailed(`Action failed: ${err}`);
+    return;
+  }
+  // jq includes the " characters around the licenses, which we don't need.
+  let matchedLicenses = jqOutput.split('\n').map(license => license.replace(/\"/g, ''));
+  return matchedLicenses
+}
+
+async function matchLicenseReferenceInFile(sourcePath, filename) {
+  let licenseeOutput;
+  let jqOutput;
+  try {
+    licenseeOutput = await runLicensee(sourcePath);
+    jqOutput = await jq.run(`.matched_files[] | select(.filename=="${filename}")`,
+      licenseeOutput, { input: 'string', output: 'string' });
+    jqOutput = await jq.run('.matched_license', jqOutput, { input: 'string', output: 'string' });
+    return jqOutput;
+  } catch (err) {
+    core.setFailed(`Action failed: ${err}`);
+    return;
+  }
+}
+
+async function verifyMatchedFiles(matchedFiles, crateName) {
+  core.info(`Verifying license files for ${crateName}.`);
+  core.info(`Licensee detected ${matchedFiles.length} files with license references:`);
+  core.info(matchedFiles)
+  if (crateName == 'root') {
+    // In this case, we're either examining the root of a workspace repo, or it's a single crate
+    // repository. We expect a LICENSE file and a README at the root, but a Cargo.toml may also be
+    // matched.
+    if (matchedFiles.length < 2) {
+      core.info('This repository is either missing a LICENSE file or no license is declared in the README.');
+      core.info('Or both.');
+      core.info('Please include a LICENSE file and a reference to it in the README.');
+      core.setFailed('Verification failed. See above.');
+      return;
+    } else if (matchedFiles.length > 3) {
+      core.info('Licensee detected more than three license references.');
+      core.info('To keep things clean, please modify the repository to:');
+      core.info('* Use one license in a LICENSE file at the root.');
+      core.info('* Make a reference to the license in the README.');
+      core.info('* If this is a Cargo repository, add a license to the Cargo manifest.');
+      core.setFailed('Verification failed. See above.');
+      return;
+    }
+    if (!matchedFiles.includes('LICENSE')) {
+      core.setFailed('Licensee did not detect a valid license in the LICENSE file.');
+      return;
+    }
+  } else {
+    // In this case, we're examining a crate within a workspace repo. We would expect at least a
+    // README, and a Cargo.toml would also be matched, but not a LICENSE file.
+    if (matchedFiles.length < 1) {
+      core.info(`${crateName} in the workspace is missing a README file with a license reference.`);
+      core.setFailed('Verification failed. See above.');
+      return;
+    }
+    if (matchedFiles.length > 2) {
+      core.info(`Licensee detected more than two license references in ${crateName}.`);
+      core.info('To keep things clean, please modify the crate to:');
+      core.info('* Make a reference to the license in the README.');
+      core.info('* If this is a Cargo repository, add a license to the Cargo manifest.');
+      core.setFailed('Verification failed. See above.');
+      return;
+    }
+  }
+  if (!matchedFiles.includes('README.md')) {
+    core.setFailed('Licensee did not detect a valid license in the README file.');
+    return;
+  }
+  core.info('License file verification passed.')
+}
+
+async function verifyLicenseFiles() {
+  const cwd = process.cwd();
+  let matchedFiles = await matchLicenseFiles(`${cwd}`);
+  await verifyMatchedFiles(matchedFiles, 'root');
+
+  if (IS_CARGO_WORKSPACE) {
+    core.info('Examining crates in the workspace:');
+    core.info(CRATES)
+    for (let i = 0; i < CRATES.length; i++) {
+      let crate = CRATES[i];
+      core.info(`Processing ${crate}...`);
+      let matchedFiles = await matchLicenseFiles(`${cwd}/${crate}`);
+      await verifyMatchedFiles(matchedFiles, crate);
+    }
+  }
+}
+
+async function verifyLicenseDetails() {
+  const cwd = process.cwd();
+  let matchedLicenses = await matchLicenseReferences(`${cwd}`);
+  core.info('Licensee detected the following license references:');
+  core.info(matchedLicenses);
+
+  let cargoLicense;
+  let licenseFileLicense = await matchLicenseReferenceInFile(`${cwd}`, 'LICENSE');
+  let readmeLicense = await matchLicenseReferenceInFile(`${cwd}`, 'README.md');
+  if (IS_CARGO_WORKSPACE) {
+    core.info(`License detected in the root LICENSE file: ${licenseFileLicense}`);
+    core.info(`License detected in the root README file: ${readmeLicense}`);
+    if (readmeLicense != licenseFileLicense) {
+      core.setFailed('The LICENSE and root README must have a matching license.');
+      return;
+    }
+
+    core.info('Examining crates in the workspace:');
+    core.info(CRATES)
+    for (let i = 0; i < CRATES.length; i++) {
+      let crate = CRATES[i];
+      core.info(`Processing ${crate}...`);
+      let cargoLicense = await matchLicenseReferenceInFile(`${cwd}/${crate}`, 'Cargo.toml');
+      let readmeLicense = await matchLicenseReferenceInFile(`${cwd}/${crate}`, 'README.md');
+      core.info(`License detected in the LICENSE file: ${licenseFileLicense}`);
+      core.info(`License detected in the README file: ${readmeLicense}`);
+      core.info(`License detected in the Cargo.toml file: ${cargoLicense}`);
+
+      if (readmeLicense == licenseFileLicense && cargoLicense == licenseFileLicense) {
+        core.info(`Licensing has been applied consistently for ${crate}.`)
+      } else {
+        core.setFailed(`The ${crate} crate failed validation: each file must have a matching license.`);
+        return;
+      }
+    }
+  } else {
+    // In the non-workspace repo, all three files are at the root of the repository.
+    cargoLicense = await matchLicenseReferenceInFile(`${cwd}`, 'Cargo.toml');
+    core.info(`License detected in the LICENSE file: ${licenseFileLicense}`);
+    core.info(`License detected in the README file: ${readmeLicense}`);
+    core.info(`License detected in the Cargo.toml file: ${cargoLicense}`);
+    if (readmeLicense == licenseFileLicense && cargoLicense == licenseFileLicense) {
+      core.info('Licensing has been applied consistently.')
+      return;
+    }
+    core.setFailed('The LICENSE, README and Cargo.toml must have a matching license.');
+  }
+}
+
+async function run() {
+  await parseInputs();
+  await buildLicensee();
+  await verifyLicenseFiles();
+  await verifyLicenseDetails();
+}
+
+run();
